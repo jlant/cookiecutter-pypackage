a43a93fe37deb19edad91f7837411a6eb54b866c
Jeremiah Lant <jeremiahlant@gmail.com>
2015-08-13 23:36:40 -0400

    fixed project short description, and removed rst extension from authors file

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/__init__.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/__init__.py
@@ -2,13 +2,13 @@
 """
     {{ cookiecutter.repo_name }}
     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    {{ cookiecutter.proj_short_description}}
+    {{ cookiecutter.project_short_description}}
 
     Nutshell
     --------
     Here a small example of using {{ cookiecutter.repo_name }}
 
-    :copyright: {{ cookiecutter.year }} by {{ cookiecutter.full_name }}, see AUTHORS.rst
+    :copyright: {{ cookiecutter.year }} by {{ cookiecutter.full_name }}, see AUTHORS
     :license: The MIT License (MIT), see LICENSE file for more details
 """
 
